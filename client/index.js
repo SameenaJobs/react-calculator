24b94c6c1bbf505eb9c1298151ffc7b41bb056cb
SameenaJobs <krsameena.chotu@gmail.com>
2018-07-09 12:45:50 +0530

    Set up webpack and test bundle js file

---
+++ client/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// using the render method, we will mount this node into our DOM (html file) on the element with 
+// id of 'app'
+ReactDOM.render(
+  <div>Steve Jobs</div>,
+  document.getElementById('app')
+);
